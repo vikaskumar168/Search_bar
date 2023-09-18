2b272e2fa125da5b4e2a0d8668d0f8503bcbbb60
Vikas <vikaskmr2411@gmail.com>
2023-09-18 23:41:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./sampledata.json";
+import './style.css';
+
+import React, {useState} from "react";
 
 function App() {
+  const [searchTerm,setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+      <div className='search_container'>
+        <input id='searchinput' type="text" placeholder='Search here... ' onChange={(e)=>{
+          setSearchTerm(e.target.value);
+        }}></input>
+      </div>
+      <h1 id='header'>List of items</h1>
+      <div className='template_container'>
+       { 
+        data.data 
+       
+        .filter((val) => {
+          if(searchTerm === ""){
+            return val;
+          }
+          else if(val.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+            return val;
+          }
+          return null;
+        })
+        .map((val) => (
+          <div className="card" >
+            <div className='template' key={val.id}>
+             <img src={val.thumbnail}alt={val.title}/>
+              <h3 className="title">{val.title}</h3>
+              <p className="desc">{val.description}</p>
+              <h2 className="brand">{val.brand}</h2>
+              <p className='price'>{val.price}</p>
+              </div>
+          </div>
+        ))
+       }
+      </div>
     </div>
+    </>
   );
 }
 
